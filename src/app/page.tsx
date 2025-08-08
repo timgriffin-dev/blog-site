763fd43ad6ddd30aa7a2ab87b5868bb9cf35d098
timgriffin-dev <tim-griffin@outlook.com>
2025-08-08 13:46:44 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <img src="/you-didnt-say-the-magic-word-ah-ah.gif" alt="Uh uh uh" className="w-80" />
+    </div>
+  );
+}
